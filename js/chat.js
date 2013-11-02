8dea0f25f3cdf10d2fea31460a69f1cfc9a17629
kmooney <kmooney@gmail.com>
2013-11-01 20:43:54 -0500

    Angular client for websocket.

--- js/chat.js
+++ js/chat.js
@@ -1,29 +1,50 @@
-var Socket = function() {
-    var server = "ws://localhost:8686";
-    var t = this;
-    this.socket = new WebSocket(server+"/chat");
 
-    this.socket.onmessage = function(evt) { 
-        console.log(JSON.parse(evt.data));
+function ChatController($scope) { 
+    Socket = function() {
+        var server = "ws://localhost:8686";
+        var t = this;
+        this.socket = new WebSocket(server+"/chat");
+
+        this.socket.onmessage = function(evt) { 
+            console.log(JSON.parse(evt.data));
+        };
+
+        this.socket.onclose = function(evt) { 
+            console.log("Connection Shut");
+        };
+
+        this.socket.onopen = function(evt) { 
+            console.log("Connection Opened!");
+            msg = JSON.stringify({
+                urlhash: String(CryptoJS.MD5(window.location.href)),
+                type: 'gimme'
+            });
+            t.socket.send(msg);
+        };
+
+        this.send = function(email, comment) { 
+            msg = JSON.stringify({
+                email: email, 
+                type: 'message',
+                message: comment,
+                urlhash: String(CryptoJS.MD5(window.location.href))
+            });
+            t.socket.send(msg);
+        };
+
     };
 
-    this.socket.onclose = function(evt) { 
-        console.log("Connection Shut");
+
+    $scope.socket = new Socket();
+
+    $scope.comments = [ 
+    ];
+
+    $scope.addComment = function() {
+        console.log("Adding a comment."); 
+        $scope.socket.send($scope.commentEmail, $scope.commentMessage);
+        $scope.commentMessage = '';
     };
 
-    this.socket.onopen = function(evt) { 
-        console.log("Connection Opened!");
-    };
 
-    this.send = function(email, comment) { 
-        msg = JSON.stringify({
-            email: email, 
-            message: comment,
-            urlhash: String(CryptoJS.MD5(window.location.href))
-        });
-        t.socket.send(msg);
-    };
-
-};
-
-s = new Socket();
+}
